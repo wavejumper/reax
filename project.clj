03bb59f0a298eebd80fb23ed2c326c1df1574a24
Thomas Crowley <crowley@kibu.com.au>
2019-11-16 12:39:41 +1100

    Add project.clj

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject wavejumper/reax "1.1.0"
+  :description "Event driven RPC between a Swift backend and a Clojurescript front end."
+  :url "https://github.com/wavejumper/reax"
+
+  :dependencies
+  [[org.clojure/clojurescript "1.10.520" :scope "provided"]]
+
+  :source-paths
+  ["src"])
